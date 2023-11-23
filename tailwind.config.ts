856dab5edec2603279e2e70c2b8f4adafff795cf
Lody Borgers <l.borgers@taf.nl>
2023-11-23 21:34:51 +0100

    Setup mini-design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,20 @@
 import type { Config } from "tailwindcss";
+import { black, white } from "tailwindcss/colors";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      black,
+      white,
+      grey: "rgb(230, 230, 230)",
+      "neon-blue": "rgb(0, 9, 255)",
+      "light-blue": "rgb(0, 151, 255)",
+    },
+    fontFamily: {
+      standard: '"Helvetica", "Arial", sans-serif',
+      plakat: '"Plakat Regular", "Helvetica", "Arial", sans-serif',
+    },
     extend: {},
   },
   plugins: [],
