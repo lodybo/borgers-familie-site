04e63b1dd1dadd30588b52ac7072e35652ebbf94
Lody Borgers <hi@lodybo.nl>
2025-02-14 11:18:24 +0100

    Improved event list where past events will not show up anymore.

--- app/components/EventsSummary.tsx
+++ app/components/EventsSummary.tsx
@@ -11,29 +11,36 @@ type Props = {
 
 export default function EventsSummary({ events }: Props) {
   return (
-    <>
+    <div className="space-y-4">
       <h2 className="text-4xl">Aankomende shows</h2>
-      <ul>
-        {events.map((event) => (
-          <li className="flex flex-col md:flex-row gap-8" key={event.id}>
-            <div className="space-y-3">
-              <h3>{event.name}</h3>
+      {events.length === 0 ? (
+        <p className="text-sm">
+          Er zijn geen aankomende shows. Houd de website goed in de gaten om te
+          weten wanneer we weer in de buurt spelen!
+        </p>
+      ) : (
+        <ul>
+          {events.map((event) => (
+            <li className="flex flex-col md:flex-row gap-8" key={event.id}>
+              <div className="space-y-3">
+                <h3>{event.name}</h3>
 
-              <EventMeta
-                startTime={event.date.toString()}
-                doorsOpen={event.doorsOpen.toString()}
-                price={event.price}
-                venue={event.venue}
-                venueAddress={event.venueAddress}
-                venueUrl={event.venueUrl}
-                ignoreStartTime={event.ignoreStartTime}
-              />
+                <EventMeta
+                  startTime={event.date.toString()}
+                  doorsOpen={event.doorsOpen.toString()}
+                  price={event.price}
+                  venue={event.venue}
+                  venueAddress={event.venueAddress}
+                  venueUrl={event.venueUrl}
+                  ignoreStartTime={event.ignoreStartTime}
+                />
 
-              <p className="text-base">{event.description}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </>
+                <p className="text-base">{event.description}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
